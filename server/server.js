7fd228147a761b3704f19450e3f5c6ae781ee34f
TomasGarciaDev <tomasgarciadev@gmail.com>
2025-03-26 13:52:27 +0100

    Feat: typeDefs

--- server/server.js
+++ server/server.js
@@ -1,7 +1,26 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-const server = new ApolloServer({});
+const typeDefs = `
+  type Query {
+    getUsers: [User]
+    getUserById(id: ID!): User
+  }
+
+  type Mutation {
+    createUser(name: String!, age: Int!, isMarried: Boolean!): User
+  }
+
+  type User {
+    id: ID
+    name: String
+    age: Int
+    isMarried: Boolean
+  }
+`;
+const resolvers = {};
+
+const server = new ApolloServer({ tyoeDefs, resolvers });
 
 const { url } = await startStandaloneServer(server, { listen: { port: 4000 } });
 
