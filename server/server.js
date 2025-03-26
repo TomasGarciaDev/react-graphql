e77cee2da54098cc4e4ded724e72a3c8a7e29180
TomasGarciaDev <tomasgarciadev@gmail.com>
2025-03-26 14:02:30 +0100

    Feat: GraphQL Resolvers

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+const users = [
+  { id: "1", name: "John Doe", age: 30, isMarried: true },
+  { id: "2", name: "Jane Smith", age: 25, isMarried: false },
+  { id: "3", name: "Alice Johnson", age: 28, isMarried: false },
+];
+
 const typeDefs = `
   type Query {
     getUsers: [User]
@@ -18,7 +24,25 @@ const typeDefs = `
     isMarried: Boolean
   }
 `;
-const resolvers = {};
+
+const resolvers = {
+  Query: {
+    getUsers: () => {
+      return users;
+    },
+    getUserById: (parent, args) => {
+      const id = args.id;
+      return users.find((user) => user.id === id);
+    },
+  },
+  Mutation: {
+    createUser: (parent, args) => {
+      const { name, age, isMarried } = args;
+      const newUser = { id: String(users.length + 1), name, age, isMarried };
+      users.push(newUser);
+    },
+  },
+};
 
 const server = new ApolloServer({ tyoeDefs, resolvers });
 
